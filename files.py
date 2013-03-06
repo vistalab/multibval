7d700dcaa2b56e944e3350cd1970704bfcc2a115
arokem <arokem@gmail.com>
2013-03-05 16:53:30 -0800

    Initial commit of stuff for multi-bval DWI.

---
+++ files.py
@@ -0,0 +1,10 @@
+import numpy as np
+import
+
+def get_bvecs(file_name='tensor.dat'):
+    """
+    Get the bvecs from the file, return a dict with keys => number of vecs and
+    values => array.
+    """
+
+    
